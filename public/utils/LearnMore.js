962a356d4ecafdc54f1c3397874bec8d2664dbdb
iulica04 <ivreme12@gmail.com>
2024-06-24 12:33:45 +0300

    add things

--- public/utils/LearnMore.js
+++ public/utils/LearnMore.js
@@ -1,19 +1,7 @@
 import { APP_PORT } from './config.js';
 
-function showSnackbar(message, messageType) {
-    var snackbar = document.getElementById("snackbar");
-    snackbar.innerHTML = message;
-    snackbar.className = "show";
 
-    // Add the message type class to the Snackbar
-    if (messageType === 'error') {
-        snackbar.className += " error";
-    } else if (messageType === 'info') {
-        snackbar.className += " info";
-    }
-
-    setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
-}
+console.log("test");
 
 // Afiseaza datele in tabelul specific PROJECTS
 function renderStats(stats, year, type) {

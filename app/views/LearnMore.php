5931511b7fec66a2ffcec61198bdeb795101779e
Pirlac Andrea-Diana <125950320+pirlacdianaA5@users.noreply.github.com>
2024-06-24 14:07:08 +0300

    update buttons

--- app/views/LearnMore.php
+++ app/views/LearnMore.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Learn More</title>
+    <script type="module" src="/RomanianDrugExplorer/public/utils/snackBar.js"></script>
     <link rel="stylesheet" href="/RomanianDrugExplorer/public/styles/style_LearnMore.css">
     <link rel="stylesheet" href="/RomanianDrugExplorer/public/styles/style_ShackBar.css">
     <link rel="stylesheet" href="/RomanianDrugExplorer/public/styles/style_Navbar.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script type="module" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script type="module" src="/RomanianDrugExplorer/public/utils/LearnMore.js"></script>
     <script type="module" src="/RomanianDrugExplorer/public/utils/Chart.js"></script>
     <meta charset="UTF-8">
 </head>
@@ -43,29 +44,32 @@
                 <div class="card_1">
                     <h1>Drug Related Confiscations Statistics:</h1>
                     <p>Here you can find the statistics about drug confiscation in Romania.</p>
-                    <button class="button button2" onclick="savePieChart('confiscations-chartpie', 'chartPie', 'png')">
-                        <a>Download PNG</a>
-                    </button>
-                    <button class="button button2" onclick="savePieChart('confiscations-chartpie','chartPie', 'svg')">
-                        <a>Download SVG</a>
-                    </button>
+                    <button class="button button2" data-chart-id="confiscations-chartpie" data-chart-type="chartPie"
+                        data-file-type="png">
+                        <a>Download PNG</a></button>
+                    <button class="button button2" data-chart-id="confiscations-chartpie" data-chart-type="chartPie"
+                        data-file-type="svg">
+                        <a>Download SVG</a></button>
                 </div>
 
                 <div class="card_2">
-                <div class="chartpie-container">
-                      <canvas id="confiscations-chartpie"></canvas>
-                </div>
-                <div class="radio-buttons">
+                    <div class="chartpie-container">
+                        <canvas id="confiscations-chartpie"></canvas>
+                    </div>
+                    <div class="radio-buttons">
                         <label>
-                            <input name="chartType" type="radio" checked onchange="updateChartPie('drug-captures','confiscations')" />
+                            <input name="chartType" type="radio" data-chart-id="drug-captures"
+                                data-chart-type="confiscations" checked />
                             <span>Drug & Captures</span>
                         </label>
                         <label>
-                            <input name="chartType" type="radio" onchange="updateChartPie('drug-grams','confiscations')" />
+                            <input name="chartType" type="radio" data-chart-id="drug-grams"
+                                data-chart-type="confiscations" />
                             <span>Drug & Grams</span>
                         </label>
                         <label>
-                            <input name="chartType" type="radio" onchange="updateChartPie('drug-tablets','confiscations')" />
+                            <input name="chartType" type="radio" data-chart-id="drug-tablets"
+                                data-chart-type="confiscations" />
                             <span>Drug & Tablets</span>
                         </label>
                     </div>
@@ -75,93 +79,96 @@
         </div>
 
         <div class="container_1">
-            <div class="card">
-                <div class="card_1">
-                    <h1>Drug Related Infractionality Statistics:</h1>
-                    <p>Here you can find the statistics about drug related infractionality in Romania.</p>
-                    <button class="button button2"
-                        onclick="saveChart('infractionality-chart' , 'chart', 'png')"><a>Download PNG</a></button>
-                    <button class="button button2"
-                        onclick="saveChart('infractionality-chart', 'chart', 'svg')"><a>Download SVG</a></button>
-
-                </div>
-
-                <div class="card_2">
-                    <div class="chart-container">
-                        <canvas id="infractionality-chart"></canvas>
-                    </div>
-                    <div class="radio-buttons">
-                        <label>
-                            <input name="chartType" type="radio" checked onchange="updateChart('gender-age','infractionality')" />
-                            <span>Gender & Age</span>
-                        </label>
-                        <label>
-                            <input name="chartType" type="radio" onchange="updateChart('penalties-situation','infractionality')" />
-                            <span>Penalties & Situation</span>
-                        </label>
-                    </div>
-                </div>
-
+    <div class="card">
+        <div class="card_1">
+            <h1>Drug Related Infractionality Statistics:</h1>
+            <p>Here you can find the statistics about drug related infractionality in Romania.</p>
+            <button class="button button2" data-chart-id="infractionality-chart" data-chart-type="chart" data-file-type="png">
+                <a>Download PNG</a>
+            </button>
+            <button class="button button2" data-chart-id="infractionality-chart" data-chart-type="chart" data-file-type="svg">
+                <a>Download SVG</a>
+            </button>
+        </div>
+        <div class="card_2">
+            <div class="chart-container">
+                <canvas id="infractionality-chart"></canvas>
+            </div>
+            <div class="radio-buttons">
+                <label>
+                    <input name="chartType" type="radio" data-chart-id="gender-age" data-chart-type="infractionality" checked />
+                    <span>Gender & Age</span>
+                </label>
+                <label>
+                    <input name="chartType" type="radio" data-chart-id="penalties-situation" data-chart-type="infractionality" />
+                    <span>Penalties & Situation</span>
+                </label>
             </div>
         </div>
+    </div>
+</div>
 
-        <div class="container_1">
-            <div class="card">
-                <div class="card_1">
-                    <h1>Drug Emergencies Statistics:</h1>
-                    <p>Here you can find the statistics about drug emergencies in Romania.</p>
-                     <button class="button button2" onclick="saveChart('emergencies-chart' , 'chart', 'png')"><a>Download PNG</a></button>
-                     <button class="button button2" onclick="saveChart('emergencies-chart', 'chart', 'svg')"><a>Download SVG</a></button>
-
-                </div>
-
-                <div class="card_2">
-                    <div class="chart-container">
-                        <canvas id="emergencies-chart"></canvas>
-                    </div>
-                    <div class="radio-buttons">
-                        <label>
-                            <input name="chartType" type="radio" checked onchange="updateChartEmergencies('gender-drug','emergencies')" />
-                            <span>Gender & Drug</span>
-                        </label>
-                        <label>
-                            <input name="chartType" type="radio" onchange="updateChartEmergencies('age-drug','emergencies')" />
-                            <span>Age & Drug</span>
-                        </label>
-                        <label>
-                            <input name="chartType" type="radio" onchange="updateChartEmergencies('emergencie-drug-canabis','emergencies')" />
-                            <span>Emergencie & Drug Canabis</span>
-                        </label>
-                        <label>
-                            <input name="chartType" type="radio" onchange="updateChartEmergencies('emergencie-drug-stimulanti','emergencies')" />
-                            <span>Emergencie & Drug Stimulati</span>
-                        </label>
-                        <label>
-                            <input name="chartType" type="radio" onchange="updateChartEmergencies('emergencie-drug-opiacee','emergencies')" />
-                            <span>Emergencie & Drug Opiacee</span>
-                        </label>
-                        <label>
-                            <input name="chartType" type="radio" onchange="updateChartEmergencies('emergencie-drug-NSP','emergencies')" />
-                            <span>Emergencie & Drug NSP</span>
-                        </label>
-                    </div>
-                </div>
-
+<div class="container_1">
+    <div class="card">
+        <div class="card_1">
+            <h1>Drug Emergencies Statistics:</h1>
+            <p>Here you can find the statistics about drug emergencies in Romania.</p>
+            <button class="button button2" data-chart-id="emergencies-chart" data-chart-type="chart" data-file-type="png">
+                <a>Download PNG</a>
+            </button>
+            <button class="button button2" data-chart-id="emergencies-chart" data-chart-type="chart" data-file-type="svg">
+                <a>Download SVG</a>
+            </button>
+        </div>
+        <div class="card_2">
+            <div class="chart-container">
+                <canvas id="emergencies-chart"></canvas>
+            </div>
+            <div class="radio-buttons">
+                <label>
+                    <input name="chartType" type="radio" data-chart-id="gender-drug" data-chart-type="emergencies" checked />
+                    <span>Gender & Drug</span>
+                </label>
+                <label>
+                    <input name="chartType" type="radio" data-chart-id="age-drug" data-chart-type="emergencies" />
+                    <span>Age & Drug</span>
+                </label>
+                <label>
+                    <input name="chartType" type="radio" data-chart-id="emergencie-drug-canabis" data-chart-type="emergencies" checked />
+                    <span>Emergencie & Drug Canabis</span>
+                </label>
+                <label>
+                    <input name="chartType" type="radio" data-chart-id="emergencie-drug-stimulanti" data-chart-type="emergencies" />
+                    <span>Emergencie & Drug Stimulanti</span>
+                </label>
+                <label>
+                    <input name="chartType" type="radio" data-chart-id="emergencie-drug-opiacee" data-chart-type="emergencies" checked />
+                    <span>Emergencie & Drug Opiacee</span>
+                </label>
+                <label>
+                    <input name="chartType" type="radio" data-chart-id="emergencie-drug-NSP" data-chart-type="emergencies" />
+                    <span>Emergencie & Drug NSP</span>
+                </label>
             </div>
         </div>
+    </div>
+</div>
 
         <div class="container_1">
             <div class="card">
                 <div class="card_1">
                     <h1>Anti-Drug Projects Statistics:</h1>
                     <p>Here you can find the statistics about projects and campaigns against drugs in Romania.</p>
-                    <button class="button button2" onclick="saveTableAsSVG('projects-table')"><a>Download
-                            SVG</a></button>
+
+                    <button class="button button2" data-chart-id="projects-table" data-chart-type="table"
+                        data-file-type="svg">
+                        <a>Download SVG</a></button>
+
                 </div>
 
                 <div class="card_2">
                     <table id="projects-table">
-                    <canvas id="my-canvas" style="display: none;"></canvas>
+                        <canvas id="my-canvas" style="display: none;"></canvas>
                         <thead>
                             <tr>
                                 <th>Tip Proiect</th>
@@ -179,37 +186,47 @@
 
         <div id="snackbar"></div>
 
-     <div class="container_2">
-    <div class="card">
-        <ul>
-            <h1>Resources:</h1>
-            <li>
-                <i class="large material-icons">library_books</i>
-                <a>Situația confiscărilor de droguri</a>
-                <button class=" button1" onclick="downloadFile('capturidroguri', 'csv')"><a>Download Report CSV</a></button>
-                <button class=" button1" onclick="openFileInNewTab('capturidroguri', 'csv')"><a>Open in New Tab</a></button>
-            </li>
-            <li>
-                <i class="large material-icons">library_books</i>
-                <a>Urgențele medicale datorate consumului de droguri</a>
-                <button class="button1" onclick="downloadFile('urgentemedicale', 'csv')"><a>Download Report CSV</a></button>
-                <button class="button1" onclick="openFileInNewTab('urgentemedicale', 'csv')"><a>Open in New Tab</a></button>
-            </li>
-            <li>
-                <i class="large material-icons">library_books</i>
-                <a>Infracționalitatea la regimul drogurilor</a>
-                <button class="button1" onclick="downloadFile('infractionalitate', 'csv')"><a>Download Report CSV</a></button>
-                <button class="button1" onclick="openFileInNewTab('infractionalitate', 'csv')"><a>Open in New Tab</a></button>
-            </li>
-            <li>
-                <i class="large material-icons">library_books</i>
-                <a>Proiectele și campaniile naționale de prevenire</a>
-                <button class="button1" onclick="downloadFile('proiectesicampanii', 'csv')"><a>Download Report CSV</a></button>
-                <button class="button1" onclick="openFileInNewTab('proiectesicampanii', 'csv')"><a>Open in New Tab</a></button>
-            </li>
-        </ul>
+        <div class="container_2">
+            <div class="card">
+                <ul>
+                    <h1>Resources:</h1>
+                    <li>
+                        <i class="large material-icons">library_books</i>
+                        <a>Situația confiscărilor de droguri</a>
+                        <button class="button1" data-action="downloadFile" data-file="capturidroguri"
+                            data-type="csv"><a>Download Report CSV</a></button>
+                        <button class="button1" data-action="openFileInNewTab" data-file="capturidroguri"
+                            data-type="csv"><a>Open in New Tab</a></button>
+                    </li>
+                    <li>
+                        <i class="large material-icons">library_books</i>
+                        <a>Urgențele medicale datorate consumului de droguri</a>
+                        <button class="button1" data-action="downloadFile" data-file="urgentemedicale"
+                        data-type="csv"><a>Download Report
+                                CSV</a></button>
+                        <button class="button1"  data-action="openFileInNewTab" data-file="urgentemedicale"
+                        data-type="csv"><a>Open in New Tab</a></button>
+                    </li>
+                    <li>
+                        <i class="large material-icons">library_books</i>
+                        <a>Infracționalitatea la regimul drogurilor</a>
+                        <button class="button1" data-action="downloadFile" data-file="infractionalitate"
+                        data-type="csv"><a>Download Report CSV</a></button>
+                        <button class="button1" data-action="openFileInNewTab" data-file="infractionalitate"
+                        data-type="csv"><a>Open in New Tab</a></button>
+                    </li>
+                    <li>
+                        <i class="large material-icons">library_books</i>
+                        <a>Proiectele și campaniile naționale de prevenire</a>
+                        <button class="button1"  data-action="downloadFile" data-file="proiectesicampanii"
+                        data-type="csv"><a>Download Report CSV</a></button>
+                        <button class="button1"  data-action="openFileInNewTab" data-file="proiectesicampanii"
+                        data-type="csv"><a>Open in New Tab</a></button>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-</div>
-</div>
 </body>
+
 </html>
